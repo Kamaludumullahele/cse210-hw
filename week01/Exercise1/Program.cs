bf982ad1f77f40e3e9dccca063db145fa61206ff
kamaludumullahele <kamalchandrakumar@gmail.comgit config --global user.email kamalchandrakumara@gmail.com>
2026-09-03 22:02:39 -0600

    Changes to the comments

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Prompt the user for their first and last name
+        // Prompt the user for their first name.
         Console.Write("Enter your first name: ");
         string firstName = Console.ReadLine();
-        //prompt the user for their last name
+        // Prompt the user for their last name.
         Console.Write("Enter your last name: ");
         string lastName = Console.ReadLine();
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");

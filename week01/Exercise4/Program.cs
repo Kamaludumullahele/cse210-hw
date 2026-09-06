a4ea139a3b4a2da96218502c7a61bed2db676655
kamaludumullahele <kamalchandrakumar@gmail.comgit config --global user.email kamalchandrakumara@gmail.com>
2026-09-06 09:43:21 -0600

    numbers list, sum and largest number

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        while (true)
+        {
+
+            Console.Write(@"Enter a number or enter '0 to quit: ");
+            string input = Console.ReadLine();
+            int value = int.Parse(input);
+            if (value == 0)
+                break;
+
+
+            else
+                numbers.Add(value);
+            Console.WriteLine("Numbers you entered: ");
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+            int sum = numbers.Sum();
+            numbers.Count();
+            double average = (double)sum / numbers.Count();
+            {
+                Console.WriteLine($"Sum: {sum}");
+                Console.WriteLine($"Average: {average}");
+            }
+            if (numbers.Count > 0)
+            {
+                int largest = numbers.Max();
+                Console.WriteLine($"The largest number is: {largest}");
+            }
+
+        }
+
+
     }
 }

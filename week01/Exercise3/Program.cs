80be177c6f0177451a4d32364c28a51e98da9116
kamaludumullahele <kamalchandrakumar@gmail.comgit config --global user.email kamalchandrakumara@gmail.com>
2026-09-05 22:22:19 -0600

    week01/Exercise3 coding

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess;
+        int attempts = 0;
+        do
+        {
+            Console.Write($"What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            guess = int.Parse(guessNumber);
+            attempts++;
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != number);
+
+        Console.WriteLine($"You guessed it in {attempts} attempts!");
     }
+
 }
